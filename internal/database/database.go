441e6e0b424034b6521b0c50d7c3cc682f16fd5d
Wyatt James <wyattsamueljames@gmail.com>
2025-12-25 15:52:35 -0700

    feat: add phone number validation for adding users

--- internal/database/database.go
+++ internal/database/database.go
@@ -2,6 +2,9 @@ package database
 
 import (
 	"database/sql"
+	"errors"
+	"regexp"
+	"strings"
 )
 
 func ConnectTo(url, driver string) (*sql.DB, error) {
@@ -13,13 +16,36 @@ func ConnectTo(url, driver string) (*sql.DB, error) {
 }
 
 func AddUser(db *sql.DB, user User) error {
-	err := execQuery(db, AddUserFull, user.FirstName, user.LastName, user.PhoneNumber)
+	cleanedFname := cleanData(user.FirstName)
+	cleanedLname := cleanData(user.LastName)
+	cleanedPhone := cleanData(user.PhoneNumber)
+
+	ok := validatePhoneNumber(cleanedPhone)
+	if !ok {
+		return errors.New("Phone number provided is invalid.")
+	}
+
+	err := execQuery(db, AddUserFull, cleanedFname, cleanedLname, cleanedPhone)
 	if err != nil {
 		return err
 	}
 	return nil
 }
 
+func validatePhoneNumber(phone string) bool {
+	// Not super confident in this expression since it is just stolen from
+	// GeeksForGeeks
+	r, err := regexp.Compile(`^[+]{1}(?:[0-9\-\(\)\/\.]\s?){6, 15}[0-9]{1}$`)
+	if err != nil {
+		return false
+	}
+
+	return r.MatchString(phone)
+}
+
+func cleanData(data string) string {
+	return strings.TrimSpace(data)
+}
 
 func execQuery(db *sql.DB, query Query, values ...any) error {
 	_, err := db.Exec(string(query), values...)
